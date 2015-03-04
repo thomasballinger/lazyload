cfb34e49a7aa84d066dedb5e3a402f60ccf47d99
llllllllll <joe@quantopian.com>
2015-03-03 23:29:36 -0500

    TST: Adds unittests.

--- test.py
+++ test.py
@@ -1,19 +1,66 @@
+import sys
+from types import ModuleType
+import unittest
 
-import time
-t0 = time.time()
+from lazyload import make_lazy, _LazyModuleMarker
 
-import lazyload; lazyload.make_lazy('requests')
 
-t1 = time.time()
-import requests
+class LazyLoadTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.modname = 'abc'
 
-t2 = time.time()
-requests.get
-t3 = time.time()
-import requests
-t4 = time.time()
+    def tearDown(self):
+        sys.modules.pop(self.modname, None)  # remove from the modules.
 
-print 'time to make requests lazyloaded:', t1 - t0
-print 'time to lazyload requests:', t2 - t1
-print 'time to really load requests:', t3 - t2
-print 'time to import requests again:', t4 - t3
+    def test_adds_to_modules(self):
+        make_lazy(self.modname)
+        abc = sys.modules.get(self.modname)
+        self.assertIsNotNone(
+            abc,
+            'make_lazy failed to add {mod} to the system modules'.format(
+                mod=self.modname,
+            ),
+        )
+        self.assertIsInstance(
+            abc, ModuleType, '{mod} is not a module'.format(mod=self.modname),
+        )
+
+    def test_is_lazy_module(self):
+        make_lazy(self.modname)
+        mod = __import__(self.modname)
+
+        self.assertIsInstance(mod, _LazyModuleMarker)
+
+    def test_no_leaking_attributes(self):
+        mod = __import__(self.modname)
+        self.assertNotIsInstance(
+            mod,
+            _LazyModuleMarker,
+            '{mod} should not be lazy yet'.format(mod=self.modname),
+        )
+        self.assertFalse(
+            hasattr(mod, '__mro__'),
+            'The module object actually has an __mro__, pick another'
+            ' attribute to test',
+        )
+
+        make_lazy(self.modname)
+        mod = __import__(self.modname)
+        self.assertIsInstance(
+            mod,
+            _LazyModuleMarker,
+            '{mod} should now be lazy'.format(mod=self.modname),
+        )
+        self.assertFalse(
+            hasattr(mod, '__mro__'),
+            '{mod} should not leak the abstraction by exposing __mro__'.format(
+                mod=self.modname,
+            ),
+        )
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner().run(
+        unittest.defaultTestLoader.loadTestsFromTestCase(LazyLoadTestCase),
+    )
