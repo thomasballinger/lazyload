83db59fed25628d82d996809d9ea9d9aef9dd1d3
llllllllll <joe@quantopian.com>
2015-03-03 23:29:07 -0500

    ENH: Hides the implementation of the LazyModule from the consumer.

--- lazyload/__init__.py
+++ lazyload/__init__.py
@@ -1,21 +1,30 @@
 import sys
+from types import ModuleType
 
-class LazyModule(object):
-    """Lazily import a module with minimal side effects"""
-    def __init__(self, module_name):
-        self.module_name = module_name
-        self.module = None
 
-    def __getattr__(self, attr):
-        if not self.module:
-            del sys.modules[self.module_name]
-            self.module = __import__(self.module_name)
-
-            # future imports will get the real module
-            sys.modules[self.module_name] = __import__(self.module_name)
-
-        return getattr(self.module, attr)
+class _LazyModuleMarker(object):
+    pass
 
 
 def make_lazy(module_name):
-    sys.modules[module_name] = LazyModule(module_name)
+    sys_modules = sys.modules  # cache in the locals
+    module = None
+
+    class LazyModule(_LazyModuleMarker):
+        """Lazily import a module with minimal side effects"""
+        def __mro__(self):
+            """Override the __mro__ to fool `isinstance`"""
+            return (LazyModule, ModuleType)
+
+        def __getattribute__(self, attr):
+            """Override __getattribute__ to hide the implementation details"""
+            nonlocal module
+            if module is None:
+                del sys_modules[module_name]
+                module = __import__(module_name)
+
+                sys_modules[module_name] = __import__(module_name)
+
+            return getattr(module, attr)
+
+    sys_modules[module_name] = LazyModule()
